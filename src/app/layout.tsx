2a4f7344d9ad73833d087b8dc3beda4aea5f4b06
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2023-08-30 11:51:45 -0400

    add force-dynamic for static generation problem with vercel deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import Container from './components/container'
-
+export const dynamic = 'force-dynamic'
 export const metadata: Metadata = {
   title: 'ePAUTA',
   description: 'La herramienta con la que salvas el semestre.'
