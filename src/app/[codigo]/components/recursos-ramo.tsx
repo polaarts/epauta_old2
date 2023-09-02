14b0fce050381955d8be7e6a04ade6a13b6ab4be
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2023-09-02 13:01:19 -0400

    add: validation

--- src/app/[codigo]/components/recursos-ramo.tsx
+++ src/app/[codigo]/components/recursos-ramo.tsx
@@ -12,11 +12,19 @@ const RecursosRamo = ({ recursos, ramos, grid, id }: { recursos: Recursos, ramos
   const codigo = pathname.split('/')[1]
   const [open, setOpen] = useState(false)
   const supabase = createClientComponentClient()
+  const [tipoError, setTipoError] = useState<string | null>(null)
+  const [anioError, setAnioError] = useState<string | null>(null)
+  const [urlError, setUrlError] = useState<string | null>(null)
+  const [tagsError, setTagsError] = useState<string | null>(null)
 
   const toggle = () => { setOpen(!open) }
 
   const recursosListHandler = async () => {
-    const recursosList = []
+    setTipoError(null)
+    setAnioError(null)
+    setUrlError(null)
+    setTagsError(null)
+
     const tipo = (document.getElementById('tipo') as HTMLInputElement).value
     const anio = (document.getElementById('anio') as HTMLInputElement).value
     const semestre = (document.getElementById('semestre') as HTMLInputElement).value
@@ -25,23 +33,47 @@ const RecursosRamo = ({ recursos, ramos, grid, id }: { recursos: Recursos, ramos
     const tags = (document.getElementById('tags') as HTMLInputElement).value
     const ramo = codigo
     const user = id
-    recursosList.push({ tipo, anio, semestre, descripcion, url, tags, ramo, user })
-    console.log(recursosList, 'sin error')
 
-    try {
-      const { error } = await supabase.from('recursos').insert(recursosList)
-      router.refresh()
-      if (error != null) {
-        console.error('Error al insertar recursos:', error)
-        console.log(recursosList, 'con error')
-      } else {
-        console.log('Recursos insertados exitosamente:', recursosList)
-        // Puedes realizar alguna acción aquí, como actualizar el estado o mostrar un mensaje de éxito
-      }
-    } catch (error) {
-      console.error('Error en la solicitud:', error)
+    let isValid = true
+
+    if (tipo.trim() === '' || tipo.trim() === null) {
+      setTipoError('El tipo es obligatorio.')
+      isValid = false
     }
-    setOpen(false)
+
+    if (anio.trim() === '' || anio.trim() === null) {
+      setAnioError('El año es obligatorio.')
+      isValid = false
+    }
+
+    if (url.trim() === '' || url.trim() === null) {
+      setUrlError('La URL es obligatoria.')
+      isValid = false
+    }
+
+    if (tags.trim() === '' || tags.trim() === null) {
+      setTagsError('Los tags son obligatorios.')
+      isValid = false
+    }
+    if (isValid) {
+      // Si todos los campos son válidos, procede con la inserción de recursos
+      const recursosList = [{ tipo, anio, url, tags, ramo, user, semestre, descripcion }]
+
+      try {
+        const { error } = await supabase.from('recursos').insert(recursosList)
+        router.refresh()
+        if (error != null) {
+          console.error('Error al insertar recursos:', error)
+          console.log(recursosList, 'con error')
+        } else {
+          console.log('Recursos insertados exitosamente:', recursosList)
+          // Puedes realizar alguna acción aquí, como actualizar el estado o mostrar un mensaje de éxito
+        }
+      } catch (error) {
+        console.error('Error en la solicitud:', error)
+      }
+      setOpen(false)
+    }
   }
 
   // useEffect(() => {
@@ -88,21 +120,55 @@ const RecursosRamo = ({ recursos, ramos, grid, id }: { recursos: Recursos, ramos
               <small>** El tipo de recurso debe ser Control, Ayudantia, Solemne, Examen u Otro.</small>
             </div>
             <div className='space-y-4 p-3 my-3 bg-gray-100 rounded'>
-              <input type='text' name='tipo' id='tipo' placeholder='* **Tipo y versión. Ej: Control 3, Examen.' className='w-full border p-2 bg-white border-black rounded' />
+              <input
+                type='text'
+                name='tipo'
+                id='tipo'
+                placeholder='* **Tipo y versión. Ej: Control 3, Examen.'
+                className={`w-full border p-2 bg-white border-black rounded ${tipoError !== null && tipoError !== undefined ? 'border-red-500' : ''
+                  }`}
+              />
+              {tipoError !== null && tipoError !== undefined && <p className='text-red-500'>{tipoError}</p>}
 
               <div className='grid grid-cols-2 gap-4'>
-                <input type='number' name='anio' placeholder='*Año' id='anio' className='border p-2 bg-white border-black rounded' />
+                <input
+                  type='number'
+                  name='anio'
+                  placeholder='*Año'
+                  id='anio'
+                  className={`border p-2 bg-white border-black rounded ${tipoError !== null && tipoError !== undefined ? 'border-red-500' : ''
+                    }`}
+                />
+
                 <select name='semestre' id='semestre' className='border border-black rounded p-2 bg-white'>
                   <option value='1'>Primer semestre</option>
                   <option value='2'>Segundo semestre</option>
                   <option value='3'>Verano</option>
                 </select>
               </div>
+
+              {anioError !== null && anioError !== undefined && <p className='text-red-500'>{anioError}</p>}
               <textarea name='descripcion' placeholder='Descripción' id='descripcion' className='border border-black rounded p-2 bg-white w-full' />
               <div className='grid grid-cols-2 gap-4'>
-                <input type='url' name='url' placeholder='*URL' id='url' className='border p-2 bg-white border-black rounded' />
-                <input type='text' name='tags' placeholder='*Tags separados por coma. Ej: "Enunciado, pauta"' id='tags' className='border p-2 bg-white border-black rounded' />
+                <input
+                  type='url'
+                  name='url'
+                  placeholder='*URL'
+                  id='url'
+                  className={`border p-2 bg-white border-black rounded ${tipoError !== null && tipoError !== undefined ? 'border-red-500' : ''
+                    }`}
+                />
+                <input
+                  type='text'
+                  name='tags'
+                  placeholder='*Tags separados por coma. Ej: "Enunciado, pauta"'
+                  id='tags'
+                  className={`border p-2 bg-white border-black rounded ${tipoError !== null && tipoError !== undefined ? 'border-red-500' : ''
+                    }`}
+                />
               </div>
+              {urlError !== null && urlError !== undefined && <p className='text-red-500'>{urlError}</p>}
+              {tagsError !== null && tagsError !== undefined && <p className='text-red-500'>{tagsError}</p>}
             </div>
 
             <button onClick={recursosListHandler} className='hover:bg-gray-200 bg-gray-100 w-full p-3 rounded text-black uppercase'>Agregar</button>
